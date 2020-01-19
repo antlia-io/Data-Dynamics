b7317d058ad84c30b6353cc13c8d94316b11e780
zeeshanuet19 <zeeshan_qureshi_1@hotmail.com>
2020-01-19 13:07:47 +0500

    commit after adding data extraction code

---
+++ coinmarket_api.js
@@ -0,0 +1,23 @@
+/* Example in Node.js ES6 using request-promise */
+
+const rp = require('request-promise');
+const requestOptions = {
+  method: 'GET',
+  uri: 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest',
+  qs: {
+    'start': '1',
+    'limit': '5000',
+    'convert': 'USD'
+  },
+  headers: {
+    'X-CMC_PRO_API_KEY': 'c8ff658d-497e-4832-b6df-d809ae1e3c02'
+  },
+  json: true,
+  gzip: true
+};
+
+rp(requestOptions).then(response => {
+  console.log('API call response:', response);
+}).catch((err) => {
+  console.log('API call error:', err.message);
+});
